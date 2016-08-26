256bdaf76c98b74f08fffc3502b1b77e97419bfa
George Matthews <gfmatthews@live.com>
2016-08-25 20:10:52 -0400

    Updating assembly info

--- ImgFlipAPI.APISource/Properties/AssemblyInfo.cs
+++ ImgFlipAPI.APISource/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ImgFlipAPI.APISource")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Creates memes using ImgFlip")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("George Matthews")]
 [assembly: AssemblyProduct("ImgFlipAPI.APISource")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
